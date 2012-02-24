747db44cf1c54cf5fd0147868e8ef6ddd80ca983
Joonha Shin <shin.joonha@gmail.com>
2012-02-24 14:00:26 -0500

    removed test cases that I didn't need

--- run.rb
+++ run.rb
@@ -1,31 +1,9 @@
 load 'lib/search_engine.rb'
 
 @se = SearchEngine.new
-chosen_article = "data/chosen_article.dat.gz"
 collection = "data/latimes_810.dat.gz"
-
 t1 = Time.now
-
-puts "Building TF Hash Table..."
-tf_hash = @se.read_gzip(chosen_article).tokenize.build_tf
-
-puts "\nTF Table"
-tf_hash.display_table
-
-puts "\nBuilding dictionary..."
-dictionary = tf_hash.build_dictionary
-
-puts "\nBuilding DF Table, please be patient..."
-df_hash = @se.build_df(dictionary, collection)
-
-puts "\nBuilding TFIDF Table..."
-tfidf_hash = @se.build_tfidf(tf_hash, df_hash)
-
-puts "\nTFIDF Table"
-@se.display_tfidf_table(tf_hash, tfidf_hash)
-
-t2 = Time.now
-puts "\nTotal processing time was #{t2 - t1} seconds."
-
 @se.write_to_file
 @se.read_from_file
+t2 = Time.now
+puts "\nTotal processing time was #{t2 - t1} seconds."
