c407222765daaba3be3743ee92f3e87744e2d26b
Joonha Shin <shin.joonha@gmail.com>
2012-02-24 14:23:23 -0500

    wrote the first RSpec test

--- spec/search_engine_spec.rb
+++ spec/search_engine_spec.rb
@@ -3,9 +3,10 @@ load 'lib/search_engine.rb'
 describe SearchEngine do
   before(:each) do
     @se = SearchEngine.new
+    @file_name = "data/latimes_1.dat.gz"
   end
 
-  it "should pass this test" do
-    true
+  it "should be able to read gzipped data" do
+    @se.read_data(@file_name).should be_true
   end
 end
